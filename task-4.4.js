3094ff9a89e8acc1e166e5e3212470003c45abaa
Filfilop <64086672+Filfilop@users.noreply.github.com>
2020-09-06 11:34:19 +0300

    дз4

--- task-4.4.js
+++ task-4.4.js
@@ -1,8 +1,8 @@
 const account = {
-  owner: 'Mango',
+  owner: "Mango",
   balance: 24000,
   discount: 0.1,
-  orders: ['order-1', 'order-2', 'order-3'],
+  orders: ["order-1", "order-2", "order-3"],
   changeDiscount(value) {
     this.discount = value; // Write code in this line
   },
@@ -16,17 +16,3 @@ const account = {
 };
 const copyAccount = Object.assign({}, account);
 copyAccount.orders = [...account.orders];
-копируем для автотестов ссылочные типы
-
-/*
-account.changeDiscount(0.15);
-//console.log(account.discount); // 0.15
-
-//console.log(account.showOrders());
-// ['order-1', 'order-2', 'order-3']
-
-account.addOrder(5000, 'order-4');
-//console.log(account.balance); // 19000
-
-//console.log(account.showOrders());
-// ['order-1', 'order-2', 'order-3', 'order-4']
